26be316bd42c14920aa7afd5ecfc8e43bcda8db6
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-11 14:58:14 -0400

    created file structure, added some JS that I know, created new branch

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,53 @@
+const concertList = [ 
+  { 
+    dateHeader: 'DATES',
+    date: 'Mon Sept 06 2021',
+    venueHeader: 'VENUE',
+    venue: 'Ronald Lane',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  },
+  {
+    dateHeader: 'DATES',
+    date: 'Tue Sept 21 2021',
+    venueHeader: 'VENUE',
+    venue: 'Pier 3 East',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  },
+  {
+    dateHeader: 'DATES',
+    date: 'Fri Oct 15 2021',
+    venueHeader: 'VENUE',
+    venue: 'View Lounge',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  },
+  {
+    dateHeader: 'DATES',
+    date: 'Sat Nov 06 2021',
+    venueHeader: 'VENUE',
+    venue: 'Hyatt Agency',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  },
+  {
+    dateHeader: 'DATES',
+    date: 'Fri Nov 26 2021',
+    venueHeader: 'VENUE',
+    venue: 'Moscow Center',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  },
+  {
+    dateHeader: 'DATES',
+    date: 'Wed Dec 15 2021',
+    venueHeader: 'VENUE',
+    venue: 'Press Club',
+    locationHeader: 'LOCATION',
+    location: 'San Francisco, CA'
+  }
+]
+
+console.log(concertList);
+//  set show array above, this is all I know what to do for now//

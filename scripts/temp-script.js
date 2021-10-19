4b294e8226c7ee4f10fb3e2becfb46f413209dd6
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-19 10:51:28 -0400

    got API call working in temp JS file, working through the data

--- scripts/temp-script.js
+++ scripts/temp-script.js
@@ -8,6 +8,22 @@
 // "api_key": "aa471e79-f431-4c44-b489-7f88a7efba89"
 // }
 
+const apiKEY = 'aa471e79-f431-4c44-b489-7f88a7efba89';
 
-const apiURL = 'aa471e79-f431-4c44-b489-7f88a7efba89';
+const apiURL = 'https://project-1-api.herokuapp.com'
+
+axios.get(`${apiURL}/showdates/?api_key=${apiKEY}`).then((response)=> {
+
+  console.log(response); // <--works
+
+  console.log(response.data); // <--works
+
+}).catch((error)=> {
+  console.log(error)
+});
+
+
+
+
+let parentItem = document.querySelector('.shows__title');
 

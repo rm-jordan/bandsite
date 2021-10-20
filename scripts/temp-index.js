0825bf34446bc05580f227dcd251b6a1144a5621
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-20 10:52:34 -0400

    started work setting up comments page to index.html

--- scripts/temp-index.js
+++ scripts/temp-index.js
@@ -1,1 +1,23 @@
 // this is where I will temporarily do the JS for the index page and comments
+
+
+
+
+const apiKEY = 'aa471e79-f431-4c44-b489-7f88a7efba89';   
+
+const apiURL = 'https://project-1-api.herokuapp.com'
+
+
+
+// lets grab the parent element
+let parentComment = document.querySelector('.comment__section'); 
+
+// make this a function:
+// remember you need to get the comments then make a seperate call in order to post the comments
+// easier anyways
+
+
+// GET
+
+
+axios.get().then().catch()

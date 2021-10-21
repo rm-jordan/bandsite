b48781dac406b2f20e76538d9d319404be49f0e2
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-21 08:45:15 -0400

    starting looping through the array in the temp js file for the index.html comment section,

--- scripts/temp-index.js
+++ scripts/temp-index.js
@@ -20,4 +20,66 @@ let parentComment = document.querySelector('.comment__section');
 // GET
 
 
-axios.get().then().catch()
+axios.get((`${apiURL}/comments/?api_key=${apiKEY}`)).then((response) => {
+// console.log(response) // <-working
+response.data.forEach((comments) => {
+  console.log(comments);
+
+
+
+
+const commentArticle = document.createElement('article');
+
+commentArticle.classList.add('comment__container');
+
+const commentImg = document.createElement('img');
+
+commentImg.classList.add('comment__img');
+
+commentImg.setAttribute('src', '../Assets/Images/default.png');
+
+// create the div - comment__profile__container
+const commentContainer = document.createElement('div')
+
+commentContainer.classList.add('comment__profile__container');
+
+
+// create the div - comment__profile__header
+const profileHeader = document.createElement('div');
+profileHeader.classList.add('comment__profile__header');
+
+
+
+// create the h4 - user - array
+const userName = document.createElement('h4')
+// console.log('h4') <--works
+// userName.innerText = comment[i].user;
+// console.log(userName); <-- works
+
+// create the p - date - array
+const userDate = document.createElement('p')
+// userDate.innerText = comment[i].date;
+
+
+// create the p - enclosed in profile header - array comments
+const userComments = document.createElement('p');
+// userComments.innerText = comment[i].comments;
+
+
+commentArticle.appendChild(commentImg); // <-- check
+commentArticle.appendChild(commentContainer); //<--check
+commentContainer.appendChild(profileHeader); // <--check
+profileHeader.appendChild(userName); // <-- check
+profileHeader.appendChild(userDate); // <--check
+commentContainer.appendChild(userComments);
+
+
+// glue everything together <---
+parentComment.appendChild(commentArticle);
+
+});
+
+}).catch((error) => {
+  console.log(error);
+});
+
